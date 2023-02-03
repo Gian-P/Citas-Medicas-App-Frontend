4e48aaeccd69df1c52ccb970192bf11f5f161d3d
Hector Almonte <hbalmontess272@gmail.com>
2023-02-02 20:12:07 -0400

    Creación de modulos

---
+++ Citas-Medicas-FE/src/app/app-child/app-child.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppChildRoutingModule } from './app-child-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppChildRoutingModule
+  ]
+})
+export class AppChildModule { }

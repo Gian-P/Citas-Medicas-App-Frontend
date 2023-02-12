8910ea29cccc42997b3d996df7a56e6443b6e7b4
Hector Almonte <hbalmontess272@gmail.com>
2023-02-12 16:22:05 -0400

    Initial Calendar

--- Citas-Medicas-FE/src/app/Feature/Pages/NotAuthorized/not-authorized.component.ts
+++ Citas-Medicas-FE/src/app/Feature/Pages/NotAuthorized/not-authorized.component.ts
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-authorized.component.scss']
 })
 export class NotAuthorizedComponent implements OnInit {
+  currentDate = new Date();
+  month = this.currentDate.toLocaleString('default', { month: 'long' });
+  year = this.currentDate.getFullYear();
+  weeks = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.generateCalendar();
   }
 
+  ngOnInit(): void {}
+
+  generateCalendar() {
+    const daysInMonth = new Date(this.year, this.currentDate.getMonth() + 1, 0).getDate();
+    const firstDayOfMonth = new Date(this.year, this.currentDate.getMonth(), 1).getDay();
+
+    let currentWeek = [];
+    let currentDay = 1;
+
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 7; j++) {
+        if (i === 0 && j < firstDayOfMonth) {
+          currentWeek.push('');
+        } else if (currentDay > daysInMonth) {
+          return;
+        } else {
+          currentWeek.push(currentDay);
+          currentDay++;
+        }
+      }
+      currentWeek = [];
+    }
+  }
+
+  nextMonth() {
+    this.currentDate = new Date(this.year, this.currentDate.getMonth() + 1, 1);
+    this.month = this.currentDate.toLocaleString('default', { month: 'long' });
+    this.year = this.currentDate.getFullYear();
+    this.weeks = [];
+    this.generateCalendar();
+  }
+
+  previousMonth() {
+    this.month = this.currentDate.toLocaleString('default', { month: 'long' });
+    this.year = this.currentDate.getFullYear();
+    this.weeks = [];
+    this.generateCalendar();
+  }
 }

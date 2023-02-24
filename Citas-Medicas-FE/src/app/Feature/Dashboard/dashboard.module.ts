b53f904152818c54f67a4622e97f63f669a71be1
Hector Almonte <hbalmontess272@gmail.com>
2023-02-23 22:14:24 -0400

    creacion de componente especialidad

--- Citas-Medicas-FE/src/app/Feature/Dashboard/dashboard.module.ts
+++ Citas-Medicas-FE/src/app/Feature/Dashboard/dashboard.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from 'src/app/Shared/shared.module';
 import { DashboardComponent } from './dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EspecialidadesComponent } from './Components/Admin/Especialidades/especialidades.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    EspecialidadesComponent
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ]
 })
 export class DashboardModule { }
